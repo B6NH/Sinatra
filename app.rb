eeec429b28bf75c98d2924a0c388cdfbc7aa1485
Jan Kowalski <yakov85@o2.pl>
2019-10-15 22:04:51 +0200

    Create Post model, connect to database, add .gitignore file

--- app.rb
+++ app.rb
@@ -2,6 +2,19 @@ require 'bundler/setup'
 require 'sinatra'
 require 'data_mapper'
 
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
+
+class Post
+    include DataMapper::Resource
+    property :id, Serial
+    property :title, String
+    property :body, Text
+    property :created_at, DateTime
+end
+
+DataMapper.finalize
+Post.auto_upgrade!
+
 get '/' do
   erb :index
 end
