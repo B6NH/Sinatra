3a52ee1cd074e38b0f356515165cdcf5f7e6d79a
Jan Kowalski <yakov85@o2.pl>
2019-10-29 16:05:47 +0100

    Unique user name

--- app.rb
+++ app.rb
@@ -46,7 +46,7 @@ class User
   include DataMapper::Resource
 
   property :id,    Serial
-  property :name , String, :required => true
+  property :name , String, :required => true, :unique => true
   property :password , String, :required => true
 end
 
